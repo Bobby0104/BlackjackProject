ebdc86567808cfefef0f2bd5d237ef402f2363fa
Bobby <robdiesel87@gmail.com>
2021-08-13 17:51:56 -0600

    initial commit

---
+++ src/com/skilldistillery/blackjack/entities/Suit.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.blackjack.entities;
+
+public enum Suit { HEARTS("Hearts"), SPADES("Spades"), CLUBS("Clubs"), DIAMONDS("Diamonds");
+	
+	private String name;
+	
+	Suit(String thePoliteNameForDisplay) {
+		this.name = thePoliteNameForDisplay;
+
+}
+}
